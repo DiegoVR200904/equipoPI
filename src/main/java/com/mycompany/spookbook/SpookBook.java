d4f631ee04343171bbf74dcb98e7a8876bb334cd
DiegoVR200904 <al200904@alumnos.uacj.mx>
2024-02-12 19:10:58 -0700

    Hola

---
+++ src/main/java/com/mycompany/spookbook/SpookBook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spookbook;
+
+/**
+ *
+ * @author ghostpatron
+ */
+public class SpookBook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
